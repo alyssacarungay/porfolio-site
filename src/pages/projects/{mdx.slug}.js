60e356344aa6de40e70ad01952c0f9b558096d44
alyssa carungay <alyssacarungay@gmail.com>
2022-02-15 19:22:42 -0600

    added gatsbyimage

--- src/pages/projects/{mdx.slug}.js
+++ src/pages/projects/{mdx.slug}.js
@@ -1,12 +1,24 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 const ProjectPost = ({data}) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.datePublished}</p>
+            <p>Posted: {data.mdx.frontmatter.datePublished}</p>
+            <GatsbyImage
+                image = {image}
+                alt = {data.mdx.frontmatter.hero_image_alt}
+            />
+            <p>
+                Photo Credit: {" "}
+                <a href={data.mdx.frontmatter.hero_image_credit_link}>
+                    {data.mdx.frontmatter.hero_image_credit_text}
+                </a>
+            </p>
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -15,13 +27,21 @@ const ProjectPost = ({data}) => {
 }
 
 export const query = graphql`
-    query MyQuery($id: String) {
+    query($id: String) {
         mdx(id: {eq: $id}) {
+            body
             frontmatter {
-                title
                 datePublished(formatString: "MMMM D, YYYY")
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                title
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
-        body
         }
     } 
 `
